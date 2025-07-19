b41cafcb1444d3c76e0d54414a67975022347b80
Satyam <www.satyam200413@gmail.com>
2025-07-19 14:46:09 +0530

    Few css updates

--- myPortfolio/src/components/About/About.jsx
+++ myPortfolio/src/components/About/About.jsx
@@ -6,9 +6,9 @@ import profileImage from '../../assets/profile.png';
 const About = () => {
   return (
     <section
-      id="about"
-      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
-    >
+  id="about"
+  className="scroll-mt-18 py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-18 md:mt-24 lg:mt-32"
+>
       <div className="flex flex-col-reverse md:flex-row justify-between items-center">
         {/* Left Side */}
         <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
