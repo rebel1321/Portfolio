51a2ef856f33398d81819fa22aac6e5ad8b80fb5
Satyam <www.satyam200413@gmail.com>
2025-08-13 17:52:16 +0530

    Updated resume

--- myPortfolio/src/components/About/About.jsx
+++ myPortfolio/src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1VmmUnjEyG5_kmEe5xQP3L1P0UCWB2CQk/view?usp=drivesdk" 
+            href="https://drive.google.com/file/d/1X_fChKLFm35ehPhZORWJWIwOKUcKdQUp/view?usp=drive_link" 
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
